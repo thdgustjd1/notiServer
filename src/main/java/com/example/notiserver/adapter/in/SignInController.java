5e16961b96bf4d07baea5867fc128c23e5dd2f10
thdgustjd1 <thdgustjd1@naver.com>
2024-05-23 19:04:02 +0900

    delete : 필요없는 클래스 삭제

--- src/main/java/com/example/notiserver/adapter/in/SignInController.java
+++
@@ -1,7 +0,0 @@
-package com.example.notiserver.adapter.in;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class SignInController {
-}
--- src/main/java/com/example/notiserver/port/in/ManualNotiUseCase.java
+++
@@ -1,4 +0,0 @@
-package com.example.notiserver.port.in;
-
-public interface ManualNotiUseCase {
-}
